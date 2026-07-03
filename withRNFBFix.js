1b242c1296012578c0248b0f4dbccfac2d0f96a4
Isaacayomi <okunlolaa3@outlook.com>
2026-07-03 11:56:12 +0100

    fix rnfb plugin to patch podfile post install instead of xcodeproj

--- withRNFBFix.js
+++ withRNFBFix.js
@@ -1,16 +1,38 @@
-const { withXcodeProject } = require("@expo/config-plugins");
+const { withDangerousMod } = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = (config) =>
-  withXcodeProject(config, (c) => {
-    const configurations = c.modResults.pbxXCBuildConfigurationSection();
-    for (const key in configurations) {
-      // Skip comment entries, which show up as string values, not objects
-      if (typeof configurations[key].buildSettings !== "undefined") {
-        configurations[
-          key
-        ].buildSettings.CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES =
-          "YES";
+module.exports = function withRNFBFix(config) {
+  return withDangerousMod(config, [
+    "ios",
+    async (config) => {
+      const podfilePath = path.join(
+        config.modRequest.platformProjectRoot,
+        "Podfile",
+      );
+      let contents = fs.readFileSync(podfilePath, "utf-8");
+
+      const snippet = `
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |config|
+        config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+      end
+    end
+`;
+
+      if (
+        !contents.includes(
+          "CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES",
+        )
+      ) {
+        contents = contents.replace(
+          /post_install do \|installer\|/,
+          `post_install do |installer|\n${snippet}`,
+        );
+        fs.writeFileSync(podfilePath, contents);
       }
-    }
-    return c;
-  });
+
+      return config;
+    },
+  ]);
+};
