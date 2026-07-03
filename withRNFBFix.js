6a640f4efeeee5f5c03cec634fa024f82c1369ba
Isaacayomi <okunlolaa3@outlook.com>
2026-07-03 11:48:23 +0100

    fix xcode build config iteration in rnfb plugin

--- withRNFBFix.js
+++ withRNFBFix.js
@@ -1,9 +1,16 @@
 const { withXcodeProject } = require("@expo/config-plugins");
+
 module.exports = (config) =>
   withXcodeProject(config, (c) => {
-    c.modResults.buildConfigurationList().forEach((cfg) => {
-      cfg.buildSettings.CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES =
-        "YES";
-    });
+    const configurations = c.modResults.pbxXCBuildConfigurationSection();
+    for (const key in configurations) {
+      // Skip comment entries, which show up as string values, not objects
+      if (typeof configurations[key].buildSettings !== "undefined") {
+        configurations[
+          key
+        ].buildSettings.CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES =
+          "YES";
+      }
+    }
     return c;
   });
